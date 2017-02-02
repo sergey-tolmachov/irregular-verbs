6659f6ac078f7568d584773ad66aabe9117ce192
yuriy.raschupkin <yuriy.raschupkin@beetroot.se>
2017-02-02 17:57:17 +0200

    Js create array

--- script/script.js
+++ script/script.js
@@ -1,11 +1,16 @@
 $(document).ready(function() { 
     var verbs = [];
+    var verb = [];
     var counter = 0;
-    $('.table-wrapper').each(function(){
-        verbs = $(this).find('.verb').text();
-        console.log(verbs);
-        
+    $('.table-wrapper .verb').each(function(){
+        verb[counter] = $(this).clone().children().remove().end().text();
+        counter++;
+        if(counter == 3){
+            verbs.push(verb);
+            counter = 0;
+        }
     });
-    
+    console.log(verbs);
+
 });
 
